49a64f5a707e86151e25887dda8599800a0d8893
AnhMH <anh.mh@moziasoft.com>
2020-05-21 10:39:15 +0700

    Modify post api

--- fuel/app/classes/model/post.php
+++ fuel/app/classes/model/post.php
@@ -17,15 +17,13 @@ class Model_Post extends Model_Abstract {
         'id',
         'cate_id',
         'name',
-        'url',
+        'slug',
         'image',
         'description',
         'detail',
-        'seo_keyword',
-        'seo_description',
         'created',
         'updated',
-        'disable'
+        'type'
     );
 
     protected static $_observers = array(
@@ -55,6 +53,7 @@ class Model_Post extends Model_Abstract {
         $self = array();
         $isNew = false;
         $time = time();
+        $type = !empty($param['type']) ? $param['type'] : 0;
         
         // Check if exist User
         if (!empty($param['id'])) {
@@ -81,7 +80,7 @@ class Model_Post extends Model_Abstract {
         // Set data
         if (!empty($param['name'])) {
             $self->set('name', $param['name']);
-            $self->set('url', \Lib\Str::convertURL($param['name']));
+            $self->set('slug', \Lib\Str::convertURL($param['name']));
         }
         if (!empty($param['cate_id'])) {
             $self->set('cate_id', $param['cate_id']);
@@ -95,12 +94,7 @@ class Model_Post extends Model_Abstract {
         if (!empty($param['image'])) {
             $self->set('image', $param['image']);
         }
-        if (!empty($param['seo_keyword'])) {
-            $self->set('seo_keyword', $param['seo_keyword']);
-        }
-        if (!empty($param['seo_description'])) {
-            $self->set('seo_description', $param['seo_description']);
-        }
+        $self->set('type', $type);
         $self->set('updated', $time);
         if ($isNew) {
             $self->set('created', $time);
@@ -133,7 +127,7 @@ class Model_Post extends Model_Abstract {
         $query = DB::select(
                 self::$_table_name.'.*',
                 array('cates.name', 'cate_name'),
-                array('cates.url', 'cate_url')
+                array('cates.slug', 'cate_url')
             )
             ->from(self::$_table_name)
             ->join('cates', 'left')
@@ -149,11 +143,6 @@ class Model_Post extends Model_Abstract {
                 $param['cate_id'] = explode(',', $param['cate_id']);
             }
             $query->where(self::$_table_name.'.cate_id', 'IN', $param['cate_id']);
-        }
-        
-        if (isset($param['disable']) && $param['disable'] != '') {
-            $disable = !empty($param['disable']) ? 1 : 0;
-            $query->where(self::$_table_name.'.disable', $disable);
         }
         
         // Pagination
@@ -181,29 +170,10 @@ class Model_Post extends Model_Abstract {
         // Get data
         $data = $query->execute()->as_array();
         $total = !empty($data) ? DB::count_last_query(self::$slave_db) : 0;
-        $newPosts = array();
-        
-        if (!empty($param['get_new_posts'])) {
-            $newPosts = self::get_all(array(
-                'limit' => 4,
-                'page' => 1
-            ));
-        }
-        $discountProducts = array();
-        if (!empty($param['get_discount_products'])) {
-            $discountProducts = Model_Product::get_all(array(
-                'is_discount' => 1,
-                'page' => 1,
-                'limit' => 6,
-                'sort' => 'discount_price-desc'
-            ));
-        }
         
         return array(
             'total' => $total,
-            'data' => $data,
-            'new_posts' => $newPosts,
-            'discount_products' => $discountProducts
+            'data' => $data
         );
     }
     
@@ -217,20 +187,19 @@ class Model_Post extends Model_Abstract {
     public static function get_detail($param)
     {
         $id = !empty($param['id']) ? $param['id'] : '';
-        $url = !empty($param['url']) ? $param['url'] : '';
+        $slug = !empty($param['slug']) ? $param['slug'] : '';
         
         $query = DB::select(
                 self::$_table_name.'.*',
                 array('cates.name', 'cate_name'),
-                array('cates.url', 'cate_url')
+                array('cates.slug', 'cate_url')
             )
             ->from(self::$_table_name)
             ->join('cates', 'LEFT')
             ->on('cates.id', '=', self::$_table_name.'.cate_id')
-            ->where(self::$_table_name.'.disable', 0)
         ;
         if (!empty($url)) {
-            $query->where(self::$_table_name.'.url', $url);
+            $query->where(self::$_table_name.'.slug', $slug);
         } else {
             $query->where(self::$_table_name.'.id', $id);
         }
